6106943b48d559bab007bf904efda1d85f6c77d2
devaldo123 <devaldomourisantaula@gmail.com>
2025-10-04 13:48:13 +0700

    Create operasi_matematika.py

---
+++ operasi_matematika.py
@@ -0,0 +1,13 @@
+# Program Operasi Aritmatika Dasar
+a = int(input("Masukkan bilangan pertama: "))
+b = int(input("Masukkan bilangan kedua: "))
+
+penjumlahan = a + b
+perkalian = a * b
+pembagian = abs(a / b)
+modulus = abs(a % b)
+
+print("Hasil Penjumlahan:", penjumlahan)
+print("Hasil Perkalian:", perkalian)
+print("Hasil Pembagian:", pembagian)
+print("Hasil Modulus:", modulus)
